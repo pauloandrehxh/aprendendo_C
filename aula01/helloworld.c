bd8d28b7ca8cb65bab066f5c09545ef9eb1b76b3
Paulo André <paulotheprogramador@gmail.com>
2025-08-13 10:44:19 -0300

    Primeiro programa em c

---
+++ aula01/helloworld.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+  printf("Hello World!\n");
+  printf("Aprendendo C");
+
+  // Em novas versões do C, o return 0 pode ser omitido
+  // Mas continua sendo uma boa prática colocar por questões de compatibiliadade
+  return 0;
+}
